f8b41e22d8968975b3aeb86e243c6bcf288be695
lumi <151774547+lumi-a@users.noreply.github.com>
2026-04-17 11:24:34 +0100

    Initial implementation of `run`

--- core-exercises.py
+++ core-exercises.py
@@ -1,1 +1,17 @@
 import numpy as np
+
+
+def run(T, initial, ws, phi=None) -> float:
+    """Returns the probability of seeing observations ws[0],ws[1],... in the GHHM given
+    by T and the initial distribution.
+    T has shape (observation, previous_state, next_state).
+    If phi is none, assumes a standard HMM.
+    """
+    if phi is None:
+        phi = np.ones(T.shape[1])
+
+    matrix_product = np.eye(T.shape[1], T.shape[2])
+    for w in ws:
+        matrix_product = matrix_product @ T[w]
+    return initial @ matrix_product @ phi
+
